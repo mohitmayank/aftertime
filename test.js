cbedd421a77c4c379598f74925566ddc1d2bcc89
Mohit Jain <mayank.mohit@gmail.com>
2017-09-24 18:27:07 +0530

    changed description of package

--- test.js
+++ test.js
@@ -3,8 +3,12 @@ var after = require('./index');
 var milliSecondsIn = after.milliSecondsIn;
 var secondsIn = after.secondsIn;
 
+console.log(after.two.days);
+
 console.log(milliSecondsIn.one.second);
 console.log(milliSecondsIn.two.seconds);
 
 console.log(secondsIn.one.hour);
 console.log(secondsIn.two.days);
+
+console.log(after);
